66557befc2bb67cbb85690cac6f32fd60e02f0b6
yasser22269 <yasser.m22291@gmail.com>
2022-10-29 20:52:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Connecting to MySQL in PHP using PDO
+
+Create a Simple Database App: Connecting to MySQL with PHP
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="Pages/create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="Pages/read.php"><strong>Read</strong></a> - find a user</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
